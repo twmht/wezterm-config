e659e1713ba32688f0b3e463dc1cc9d94bfe428b
alec.tu <alec.tu@acer.com>
2025-03-10 21:08:04 +0800

    fix(config): update font size and default program for Windows
    Updated the default font size for non-macOS platforms to 12 for consistency.
    Changed the default shell in Windows from PowerShell to WSL with Ubuntu.
    Updated the Git Bash path to use a more generic installation location.
    
    These changes ensure a more consistent user experience across different platforms.

--- config/launch.lua
+++ config/launch.lua
@@ -6,7 +6,7 @@ local options = {
 }
 
 if platform.is_win then
-   options.default_prog = { 'pwsh', '-NoLogo' }
+   options.default_prog = { 'wsl', '-d', 'Ubuntu'}
    options.launch_menu = {
       { label = 'PowerShell Core', args = { 'pwsh', '-NoLogo' } },
       { label = 'PowerShell Desktop', args = { 'powershell' } },
@@ -15,7 +15,7 @@ if platform.is_win then
       { label = 'Msys2', args = { 'ucrt64.cmd' } },
       {
          label = 'Git Bash',
-         args = { 'C:\\Users\\kevin\\scoop\\apps\\git\\current\\bin\\bash.exe' },
+         args = { 'C:/Program Files/Git/bin/bash' },
       },
    }
 elseif platform.is_mac then
